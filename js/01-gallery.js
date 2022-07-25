29d5c805e4938b3371291fc2d63c276094cad1d3
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-07-25 21:43:05 +0300

    add render image

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// =========== Создание и рендер разметки по массиву данных =========== //
+
+const galleryBox = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryBox.insertAdjacentHTML("beforeend", itemsMarkup);
+
+galleryBox.addEventListener("click", onGalleryBoxClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+
+// Реализация делегирования на div.gallery и
+// получение url большого изображения.
+
+function onGalleryBoxClick(event) {
+  event.preventDefault();
+
+  const isImageGalary = event.target.classList.contains("gallery__image");
+
+  if (!isImageGalary) {
+    return;
+  }
+  console.log(event.target);
+
+  console.log(event.target.dataset.source);
+}
